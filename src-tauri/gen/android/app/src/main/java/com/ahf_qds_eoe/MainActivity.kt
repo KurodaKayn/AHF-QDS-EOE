9555abdf4ca1a8749629b5d536c8b07c9eb532ba
Kuroda Kayn <kurodakayn@outlook.com>
2026-01-12 16:07:08 +0800

    chore: release v1.2.3 with updated workflow and icons

---
+++ src-tauri/gen/android/app/src/main/java/com/ahf_qds_eoe/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ahf_qds_eoe
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
